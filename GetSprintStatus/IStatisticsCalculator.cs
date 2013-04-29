cd467b306e0cf035046f5baee90441a64799fc8f
Albert Cheng <gongchen@microsoft.com>
2013-04-29 15:48:21 -0700

    adds a missing file.

---
+++ GetSprintStatus/IStatisticsCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GHSprintTrax.GithubApi;
+using GetSprintStatus.Formatting;
+
+namespace GetSprintStatus
+{
+    interface IStatisticsCalculator
+    {
+        void Start(string message);
+
+        void StartRepository(string title, string milestone);
+
+        void AddIssue(Issue issue, float devEstimate, float testEstimate);
+
+        void AddError(Issue issue, string reason);
+
+        void Accept(IFormatter formatter);
+
+        IEnumerable<ParseError> Errors { get; }
+
+        string RepoName { get; }
+        string Milestone { get; }
+        string HeaderMessage { get; }
+    }
+}
